3e9a8b386cf671021364a06dd4fec2a9e49674d9
Dan Morton <dmorton2297@gmail.com>
2019-04-30 20:35:36 -0400

    Nav bar updates

---
+++ src/app/color.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ColorService {
+
+  constructor() { }
+
+  // https://stackoverflow.com/questions/35969656/how-can-i-generate-the-opposite-color-according-to-current-color
+  public invertColor(hex) {
+    if (hex.indexOf('#') === 0) {
+        hex = hex.slice(1);
+    }
+    // convert 3-digit hex to 6-digits.
+    if (hex.length === 3) {
+        hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2];
+    }
+    if (hex.length !== 6) {
+        throw new Error('Invalid HEX color.');
+    }
+    // invert color components
+    var r = (255 - parseInt(hex.slice(0, 2), 16)).toString(16),
+        g = (255 - parseInt(hex.slice(2, 4), 16)).toString(16),
+        b = (255 - parseInt(hex.slice(4, 6), 16)).toString(16);
+    // pad each with zeros and return
+    return '#' + this.padZero(r, r.length) + this.padZero(g, g.length) + this.padZero(b, b.length);
+  }
+
+  // https://stackoverflow.com/questions/35969656/how-can-i-generate-the-opposite-color-according-to-current-color
+  public padZero(str, len) {
+    len = len || 2;
+    var zeros = new Array(len).join('0');
+    return (zeros + str).slice(-len);
+  }
+
+  public componentToHex(c) {
+    var hex = c.toString(16);
+    return hex.length == 1 ? "0" + hex : hex;
+}
+
+public rgbToHex(r, g, b) {
+    return "#" + this.componentToHex(r) + this.componentToHex(g) + this.componentToHex(b);
+}
+
+}
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -12,15 +12,13 @@ export class HomeComponent {
 
   constructor() {}
 
-  public headerNames : string[] = ['home', 'about', 'contact'];
-  public dropHeaders : string[] = ['logout', 'settings'];
+  public headerNames : string[] = ['Home', 'Import Data', 'Export Data', 'Reports'];
+  public dropHeaders : string[] = ['Logout', 'Settings'];
   public dhandlers = [this.test, this.test];
-
-
-  public logoURL: string = 'https://avatars1.githubusercontent.com/u/23268332?s=400&v=4';
-  public handlers = [this.test, this.test, this.test];
-  public prim = "black";
-  public sec = "goldenrod";
+  public logoURL: string = 'https://avatars3.githubusercontent.com/u/46693979?s=400&u=1b0b543a9496b5746178ca58665da632282a9220&v=4';
+  public handlers = [this.test, this.test, this.test, this.test];
+  public prim = "#000219";
+  public sec = "#1A1F23";
   
   test() {
     window.alert("test ");
